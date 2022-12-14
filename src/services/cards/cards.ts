3d445c9101db824a98d5af31f783b76b31d1bfdc
anastasiakozlova <kozlova.an.dm@gmail.com>
2022-12-14 16:07:46 +0100

    Added high priestess card

--- src/services/cards/cards.ts
+++ src/services/cards/cards.ts
@@ -1,7 +1,8 @@
 import { fool } from './lib/fool/card';
 import { magician } from './lib/magician/card';
+import { highPriestess } from './lib/highPriestess/card';
 import { CardData } from './types';
 
 export function initializeGardsData(): CardData[] {
-    return [fool, magician];
+    return [fool, magician, highPriestess];
 }
