e84733291c18183548decb3207121e4d87a438da
anastasiakozlova <kozlova.an.dm@gmail.com>
2022-12-19 19:59:42 +0100

    Add files to Hierophant card

--- src/services/cards/cards.ts
+++ src/services/cards/cards.ts
@@ -1,8 +1,9 @@
 import { fool } from './lib/fool/card';
 import { magician } from './lib/magician/card';
 import { highPriestess } from './lib/highPriestess/card';
+import { hierophant } from './lib/hierophant/card';
 import { CardData } from './types';
 
 export function initializeGardsData(): CardData[] {
-    return [fool, magician, highPriestess];
+    return [fool, magician, highPriestess, hierophant];
 }
