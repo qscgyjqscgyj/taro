f62d0f062d4c78916f5835cb2aad492bcd709627
anastasiakozlova <kozlova.an.dm@gmail.com>
2022-12-19 20:46:03 +0100

    Add Empress and Emperor cards

--- src/services/cards/cards.ts
+++ src/services/cards/cards.ts
@@ -1,9 +1,11 @@
 import { fool } from './lib/fool/card';
 import { magician } from './lib/magician/card';
 import { highPriestess } from './lib/highPriestess/card';
+import { empress } from './lib/empress/card';
+import { emperor } from './lib/emperor/card';
 import { hierophant } from './lib/hierophant/card';
 import { CardData } from './types';
 
 export function initializeGardsData(): CardData[] {
-    return [fool, magician, highPriestess, hierophant];
+    return [fool, magician, highPriestess, empress, emperor, hierophant];
 }
