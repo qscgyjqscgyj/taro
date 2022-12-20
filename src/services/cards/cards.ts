20bebfc50b387dd3ecfb133eff1170b33521f2b7
anastasiakozlova <kozlova.an.dm@gmail.com>
2022-12-20 14:57:12 +0100

    Added cards: Lovers, Chariot, Strength and Hermit. Corrected Emperor card

--- src/services/cards/cards.ts
+++ src/services/cards/cards.ts
@@ -4,8 +4,23 @@ import { highPriestess } from './lib/highPriestess/card';
 import { empress } from './lib/empress/card';
 import { emperor } from './lib/emperor/card';
 import { hierophant } from './lib/hierophant/card';
+import { lovers } from './lib/lovers/card';
+import { chariot } from './lib/chariot/card';
+import { strength } from './lib/strength/card';
+import { hermit } from './lib/hermit/card';
 import { CardData } from './types';
 
 export function initializeGardsData(): CardData[] {
-    return [fool, magician, highPriestess, empress, emperor, hierophant];
+    return [
+        fool,
+        magician,
+        highPriestess,
+        empress,
+        emperor,
+        hierophant,
+        lovers,
+        chariot,
+        strength,
+        hermit,
+    ];
 }
